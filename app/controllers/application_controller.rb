3319401f9d6f7836cfb6eccdd947b0ba86645fe6
Jeffrey Tong <trigun0x2@gmail.com>
2013-05-04 19:51:24 -0400

    homepage added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  def not_authenticated
+    redirect_to login_url, :alert => "First log in to view this page."
+  end
 end
