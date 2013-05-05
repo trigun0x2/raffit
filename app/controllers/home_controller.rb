1f46dd99f66fb35bf05cb1348bb9a1d1796ba1e4
Stephen He <yihehe@gmail.com>
2013-05-05 09:34:21 -0400

    try  this

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
 	def index
-		@photos = Photo.get(:all, :order => "id desc", :limit => 20)
+		@photos = Photo.find(:all, :order => "id desc", :limit => 20)
 	end
 end
