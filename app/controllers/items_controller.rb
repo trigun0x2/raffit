025699bc78f221f601660371dcf82aa71b5eff88
James Zhang <ag.jameszhang@gmail.com>
2013-05-05 03:44:26 -0400

    fixed even more

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -52,15 +52,15 @@ class ItemsController < ApplicationController
 
   end
 
-  def random_num
-    test = Item.find(params[:id])#find()ActiveRecord::Base.connection.execute("SELECT winning_ticket FROM items WHERE id='" + params[:id] + "'")
+  def random_num(id)
+    test = Item.find(id)#find()ActiveRecord::Base.connection.execute("SELECT winning_ticket FROM items WHERE id='" + params[:id] + "'")
     if (test.winning_ticket != nil)
       return test.winning_ticket
     else
-      sum = Ticket.where(:item_id => params[:id]).count#ActiveRecord::Base.connection.execute("SELECT COUNT(*) FROM tickets WHERE item_id='" + params[:id] + "'")
+      sum = Ticket.where(:item_id => id).count#ActiveRecord::Base.connection.execute("SELECT COUNT(*) FROM tickets WHERE item_id='" + params[:id] + "'")
       random = rand(sum[0][0])
-      win = Ticket.limit(1).offset(random).find(params[:id]).id#ActiveRecord::Base.connection.execute("SELECT id FROM tickets WHERE item_id='" + params[:id] + "' LIMIT 1 OFFSET " + random.to_s)
-      c1 = Item.find(params[:id])#ActiveRecord::Base.connection.execute("UPDATE items SET winning_ticket=" + win[0][0].to_s + " WHERE id=" + params[:id])
+      win = Ticket.limit(1).offset(random).find(id).id#ActiveRecord::Base.connection.execute("SELECT id FROM tickets WHERE item_id='" + params[:id] + "' LIMIT 1 OFFSET " + random.to_s)
+      c1 = Item.find(id)#ActiveRecord::Base.connection.execute("UPDATE items SET winning_ticket=" + win[0][0].to_s + " WHERE id=" + params[:id])
       c1.winning_ticket = win[0][0]
       c1.save
       return win[0][0]
